26ae2e9391695d7f6a6cdaa81410b23730fbd83f
Bill Smart <bill.smart@oregonstate.edu>
2025-05-12 00:20:03 -0700

    Initial commit.

---
+++ rob599_stage/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+import os
+from glob import glob
+
+package_name = 'rob599_stage'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # Launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.xml'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.yaml'))),
+
+        # Config files.
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wds',
+    maintainer_email='bill.smart@oregonstate.edu',
+    description='TODO: Package description',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
